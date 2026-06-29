43ead5b468b2ec681c98074cf3c21e5dc1c21963
Neil Borikar <neilborikar25@gmail.com>
2026-06-29 22:28:51 +0530

    fixed url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Customer, Employee, InventoryItem, SalesReport } from './types';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://syncommerce.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
